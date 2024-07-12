b3863828f51e5832afe5d67add39f8719f74312b
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-07-12 22:49:06 +0300

    Add course interactions to the render api

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,6 +1,11 @@
 export interface IAPI {
   startOllama: () => Promise<void>;
   pullOllama: (model: string) => Promise<void>;
+  addCourse: (courseTitle: string) => Promise<void>;
+  removeCourse: (courseId: string) => Promise<void>;
+  getCourse: (courseId: string) => Promise<void>;
+  getCourses: () => Promise<void>;
+  updateCourse: (course: course) => Promise<void>;
 }
 
 declare global {
--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -1,6 +1,15 @@
 import { contextBridge, ipcRenderer } from "electron";
+import { type course } from "../main/courses/courses";
 
 contextBridge.exposeInMainWorld("api", {
   startOllama: () => ipcRenderer.invoke("ollama:start"),
   pullOllama: (model: string) => ipcRenderer.invoke("ollama:pull", model),
+  stopOllama: () => ipcRenderer.invoke("ollama:stop"),
+  addCourse: (courseTitle: string) =>
+    ipcRenderer.invoke("course:add", courseTitle),
+  removeCourse: (courseId: string) =>
+    ipcRenderer.invoke("course:remove", courseId),
+  getCourse: (courseId: string) => ipcRenderer.invoke("course:get", courseId),
+  getCourses: () => ipcRenderer.invoke("course:get"),
+  updateCourse: (course: course) => ipcRenderer.invoke("course:update", course),
 });
