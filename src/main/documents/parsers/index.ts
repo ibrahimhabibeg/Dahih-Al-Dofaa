42eef09090744a59f29cc855cdd11f570ef5ff2b
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-07-14 16:43:36 +0300

    Add support for parsing DOCX documents

--- src/main/documents/parsers/index.ts
+++ src/main/documents/parsers/index.ts
@@ -1,11 +1,14 @@
 import parsePdf from "./pdf";
 import parsePPTX from "./pptx";
+import parseDOCX from "./docx";
 
 async function parseDocument(document: Doc): Promise<string> {
   if (document.docType === "pdf") {
     return parsePdf(document);
   } else if (document.docType === "pptx") {
     return parsePPTX(document);
+  } else if (document.docType === "docx") {
+    return parseDOCX(document);
   }
   return "";
 }
