a02ca19c84cd71ad91108a835c0338f683833060
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-07-24 23:21:01 +0300

    Add desktop icon for linux

--- forge.config.ts
+++ forge.config.ts
@@ -1,22 +1,34 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
+import path from "path";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: path.join(process.cwd(), "public", "icon.png"),
+    extraResource: [path.join(process.cwd(), "public", "icon.png")],
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({
+      options: {
+        icon: path.join(process.cwd(), "public", "icon.png"),
+      },
+    }),
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
@@ -25,11 +37,11 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/renderer/index.html',
-            js: './src/renderer/renderer.ts',
-            name: 'main_window',
+            html: "./src/renderer/index.html",
+            js: "./src/renderer/renderer.ts",
+            name: "main_window",
             preload: {
-              js: './src/renderer/preload.ts',
+              js: "./src/renderer/preload.ts",
             },
           },
         ],
--- src/main/index.ts
+++ src/main/index.ts
@@ -26,6 +26,7 @@ const createWindow = (): void => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
+    icon: "../../public/icon.png",
   });
 
   // and load the index.html of the app.
